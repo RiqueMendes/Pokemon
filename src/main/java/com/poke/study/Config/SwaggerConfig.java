2160dc74bbe6efd255d8b20302caf6b139fd04e9
RiqueMendes <97553942+RiqueMendes@users.noreply.github.com>
2022-05-22 16:57:45 -0300

    SwaggerConfig

---
+++ src/main/java/com/poke/study/Config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.poke.study.Config;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springStudyAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Project Pokemon")
+                        .description("Project for study Spring Boot")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("HenriqueMendes")
+                                .url("https://github.com/RiqueMendes"))
+                        .contact(new Contact()
+                                .name("Repository GitHub")
+                                .url("https://github.com/RiqueMendes/Pokemon")
+                                .email("riquesmendes@gmail.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/RiqueMendes"));
+    }
+
+    private ApiResponse createApiResponse(String message) {
+        return new ApiResponse().description(message);
+    }
+
+    @Bean
+    public OpenApiCustomiser customerGlobalResponseStatus() {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+                ApiResponses api = operation.getResponses();
+
+                api.addApiResponse("200", createApiResponse("Sucess!"));
+                api.addApiResponse("201", createApiResponse("Created!"));
+                api.addApiResponse("400", createApiResponse("Request error!"));
+                api.addApiResponse("401", createApiResponse("Not authorized!"));
+                api.addApiResponse("500", createApiResponse("Internal server Error!"));
+            }));
+        };
+    }
+}
+
--- src/main/java/com/poke/study/Service/PokeService.java
+++ src/main/java/com/poke/study/Service/PokeService.java
@@ -1,13 +1,7 @@
 package com.poke.study.Service;
 
 
-import java.util.Optional;
 
-import com.poke.study.Model.Pokemon;
-import com.poke.study.Repository.PokeRepository;
-
-import org.hibernate.ObjectNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
